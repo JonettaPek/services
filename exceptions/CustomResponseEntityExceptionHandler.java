86b52a24662e49f1f5d2d2b8dd14b19589d54ae6
Jonetta <jpek009@e.ntu.edu.sg>
2023-10-27 02:51:20 +0800

    implement exception handlers

---
+++ exceptions/CustomResponseEntityExceptionHandler.java
@@ -0,0 +1,46 @@
+package com.fdmgroup.restservice.exceptions;
+
+import java.time.LocalDateTime;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<ErrorDetails> handleAllExceptions(Exception ex, WebRequest request) throws Exception {
+		ErrorDetails formattedError = new ErrorDetails(LocalDateTime.now(),
+				ex.getMessage(),
+				request.getDescription(false));
+		return new ResponseEntity<ErrorDetails>(formattedError, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@ExceptionHandler(UserNotFoundException.class)
+	public final ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex, WebRequest request) throws Exception {
+		ErrorDetails formattedError = new ErrorDetails(LocalDateTime.now(),
+				ex.getMessage(),
+				request.getDescription(false));
+		return new ResponseEntity<ErrorDetails>(formattedError, HttpStatus.NOT_FOUND);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		ErrorDetails formattedError = new ErrorDetails(LocalDateTime.now(),
+				ex.getAllErrors()
+				.stream()
+				.map(error -> error.getDefaultMessage())
+				.collect(Collectors.joining(", ")),
+				request.getDescription(false));
+		 return new ResponseEntity<>(formattedError, HttpStatus.NOT_FOUND);
+	}
+}
--- exceptions/UserNotFoundException.java
+++ exceptions/UserNotFoundException.java
@@ -3,32 +3,15 @@ package com.fdmgroup.restservice.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-public class UserNotFoundException extends Exception {
+@ResponseStatus(code=HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public UserNotFoundException() {
-		super();
-	}
-
-	public UserNotFoundException(String message, Throwable cause, boolean enableSuppression,
-			boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-	}
-
-	public UserNotFoundException(String message, Throwable cause) {
-		super(message, cause);
-	}
-
 	public UserNotFoundException(String message) {
 		super(message);
-	}
-
-	public UserNotFoundException(Throwable cause) {
-		super(cause);
 	}
 }
