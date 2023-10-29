7f4d02a8d6e7865d681c83ec9da5e0c5c64aadd2
Jonetta <jpek009@e.ntu.edu.sg>
2023-10-29 18:28:43 +0800

    configure springdoc openapi

--- controllers/HelloWorldController.java
+++ controllers/HelloWorldController.java
@@ -2,13 +2,17 @@ package com.fdmgroup.restservice.controllers;
 
 import java.time.LocalDate;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fdmgroup.restservice.dao.User;
 
 @RestController
+@RequestMapping("/hello-world")
 public class HelloWorldController {
 
 	@GetMapping(path="/greet")
