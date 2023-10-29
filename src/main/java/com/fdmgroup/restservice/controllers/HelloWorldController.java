1c041b611285be0e9254c913adf3c5edf2f749c8
Jonetta <jpek009@e.ntu.edu.sg>
2023-10-29 19:19:07 +0800

    add messages.properties for internalisation purposes

--- src/main/java/com/fdmgroup/restservice/controllers/HelloWorldController.java
+++ src/main/java/com/fdmgroup/restservice/controllers/HelloWorldController.java
@@ -1,12 +1,13 @@
 package com.fdmgroup.restservice.controllers;
 
 import java.time.LocalDate;
+import java.util.Locale;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fdmgroup.restservice.dao.User;
@@ -14,10 +15,22 @@ import com.fdmgroup.restservice.dao.User;
 @RestController
 @RequestMapping("/hello-world")
 public class HelloWorldController {
+	
+	private MessageSource messageSource;
+	
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 
 	@GetMapping(path="/greet")
 	public String greet() {
 		return "Hello World!";
+	}
+	
+	@GetMapping(path="/greet-i18n")
+	public String i18nGreet() {
+		Locale locale = LocaleContextHolder.getLocale();
+		return this.messageSource.getMessage("good.morning.message", null, "Default Message", locale);
 	}
 	
 	@GetMapping(path="/greet/{name}")
