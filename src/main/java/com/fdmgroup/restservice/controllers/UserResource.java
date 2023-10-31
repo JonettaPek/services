12c2b7ca6cb17f05a627ae468fc219f00cdb04c0
Jonetta <jpek009@e.ntu.edu.sg>
2023-10-31 17:56:47 +0800

    implement hateoas for get a user servlet method

--- src/main/java/com/fdmgroup/restservice/controllers/UserResource.java
+++ src/main/java/com/fdmgroup/restservice/controllers/UserResource.java
@@ -1,16 +1,19 @@
 package com.fdmgroup.restservice.controllers;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
 import java.net.URI;
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -36,8 +39,11 @@ public class UserResource {
 	}
 	
 	@GetMapping(path="/static-users/{id}")
-	public User getUser(@PathVariable final int id) throws UserNotFoundException {
-		return userService.find(id);
+	public EntityModel<User> getUser(@PathVariable final int id) throws UserNotFoundException {
+		EntityModel<User> entityModel = EntityModel.of(userService.find(id));
+		WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).getAllUsers());
+		entityModel.add(link.withRel("all-users"));
+		return entityModel;
 	}
 	
 	@PostMapping(path="/static-users")
