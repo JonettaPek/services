10f986e49f2b24def04b53bb4aa082e0374ad0e3
Jonetta <jpek009@e.ntu.edu.sg>
2023-10-31 17:22:55 +0800

    add different versioning methods

--- src/main/java/com/fdmgroup/restservice/controllers/PersonResourceVersioning.java
+++ src/main/java/com/fdmgroup/restservice/controllers/PersonResourceVersioning.java
@@ -7,17 +7,47 @@ import com.fdmgroup.restservice.dao.Name;
 import com.fdmgroup.restservice.dao.PersonV1;
 import com.fdmgroup.restservice.dao.PersonV2;
 
+
 @RestController
 public class PersonResourceVersioning {
 	
 	@GetMapping(path="/v1/person")
-	public PersonV1 getPersonV1() {
+	public PersonV1 getPersonV1URI() {
 		return new PersonV1("John Doe");
 	}
 	
 	@GetMapping(path="/v2/person")
-	public PersonV2 getPersonV2() {
+	public PersonV2 getPersonV2URI() {
 		return new PersonV2(new Name("John", "Doe"));
 	}
 
+	@GetMapping(path="/person", params="version=1")
+	public PersonV1 getPersonV1Param() {
+		return new PersonV1("John Doe");
+	}
+	
+	@GetMapping(path="/person", params="version=2")
+	public PersonV2 getPersonV2Param() {
+		return new PersonV2(new Name("John", "Doe"));
+	}
+	
+	@GetMapping(path="/person", headers="X-API-VERSION=1")
+	public PersonV1 getPersonV1Header() {
+		return new PersonV1("John Doe");
+	}
+	
+	@GetMapping(path="/person", headers="X-API-VERSION=2")
+	public PersonV2 getPersonV2Header() {
+		return new PersonV2(new Name("John", "Doe"));
+	}
+	
+	@GetMapping(path="/person", produces="application/vnd.company.app-v1+json")
+	public PersonV1 getPersonV1AcceptHeader() {
+		return new PersonV1("John Doe");
+	}
+	
+	@GetMapping(path="/person", produces="application/vnd.company.app-v2+json")
+	public PersonV2 getPersonV2AcceptHeader() {
+		return new PersonV2(new Name("John", "Doe"));
+	}
 }
