f726731fde2278ec19608c8e45cf43e22138b78d
Jonetta <jpek009@e.ntu.edu.sg>
2023-11-06 00:03:54 +0800

    prepopulate user details table

---
+++ rest-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO user_details (id, name, birthdate) VALUES (10001,'Sam', CURRENT_DATE());
+INSERT INTO user_details (id, name, birthdate) VALUES (10002,'Paul', CURRENT_DATE());
+INSERT INTO user_details (id, name, birthdate) VALUES (10003,'John', CURRENT_DATE());
