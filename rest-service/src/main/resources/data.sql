55c5ff5c24ea7c230eb19021902f97003a689d4e
Jonetta <jpek009@e.ntu.edu.sg>
2023-11-06 15:46:44 +0800

    prepopulate post table with initial data

--- rest-service/src/main/resources/data.sql
+++ rest-service/src/main/resources/data.sql
@@ -2,6 +2,6 @@ INSERT INTO user_details (id, name, birthdate) VALUES (10001, 'Sam', CURRENT_DAT
 INSERT INTO user_details (id, name, birthdate) VALUES (10002, 'Paul', CURRENT_DATE());
 INSERT INTO user_details (id, name, birthdate) VALUES (10003, 'John', CURRENT_DATE());
 
-INSERT INTO post (id, description, user_id) VALUES (1, 'today''s weather is very hot', 10001);
-INSERT INTO post (id, description, user_id) VALUES (2, 'hope everyone has a great day', 10001);
-INSERT INTO post (id, description, user_id) VALUES (3, 'can''t wait to start placement', 10002);
+INSERT INTO post (id, description, user_id) VALUES (20001, 'today''s weather is very hot', 10001);
+INSERT INTO post (id, description, user_id) VALUES (20002, 'hope everyone has a great day', 10001);
+INSERT INTO post (id, description, user_id) VALUES (20003, 'can''t wait to start placement', 10002);
