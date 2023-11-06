2dd611a91293ed96f4783b1fed9bedcce9e3421a
Jonetta <jpek009@e.ntu.edu.sg>
2023-11-06 09:53:45 +0800

    create post entity

---
+++ rest-service/src/main/java/com/fdmgroup/restservice/dao/Post.java
@@ -0,0 +1,54 @@
+package com.fdmgroup.restservice.dao;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Post {
+
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	private String description;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JsonIgnore
+	private User user;
+
+	public Post() {
+		super();
+	}
+	
+	public Post(long id, String description, User user) {
+		super();
+		this.id = id;
+		this.description = description;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public String toString() {
+		return "Post [id=" + id + ", description=" + description + "]";
+	}
+}
