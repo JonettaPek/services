12ca60619bd71d1595cdfc72c9efce91d931acc8
Pek, Jonetta <jpek009@e.ntu.edu.sg>
2023-11-12 01:48:17 +0800

    prepopulate currency exchange table with initial data

---
+++ spring-cloud-config/currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO currency_exchange (id, from_currency, to_currency, conversion_multiple, environment) VALUES (1000, 'USD', 'SGD', 50, '');
+INSERT INTO currency_exchange (id, from_currency, to_currency, conversion_multiple, environment) VALUES (1001, 'EUR', 'SGD', 60, '');
+INSERT INTO currency_exchange (id, from_currency, to_currency, conversion_multiple, environment) VALUES (1002, 'AUD', 'SGD', 70, '');
