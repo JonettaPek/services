048c280b6ff83b32a52f20fe1060aa9e040b57b3
Pek, Jonetta <jpek009@e.ntu.edu.sg>
2023-11-12 01:47:38 +0800

    add environment port to response

--- spring-cloud-config/currency-exchange-service/src/main/java/com/fdmgroup/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ spring-cloud-config/currency-exchange-service/src/main/java/com/fdmgroup/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fdmgroup.microservices.currencyexchangeservice.dao.CurrencyExchange;
+import com.fdmgroup.microservices.currencyexchangeservice.exception.CurrencyExchangeNotFoundException;
+import com.fdmgroup.microservices.currencyexchangeservice.repository.CurrencyExchangeRepository;
 
 @RestController
 @RequestMapping(path = "/currency-exchange")
@@ -18,10 +20,16 @@ public class CurrencyExchangeController {
 	@Autowired
 	private Environment environment;
 	
+	@Autowired
+	private CurrencyExchangeRepository currencyExchangeRepository;
+	
 	@GetMapping(path = "/from/{fromCurrency}/to/{toCurrency}")
 	public CurrencyExchange getExchangeRate(@PathVariable final String fromCurrency,
-			@PathVariable final String toCurrency) {
-		CurrencyExchange currencyExchange = new CurrencyExchange(1_000L, fromCurrency, toCurrency, BigDecimal.valueOf(50));
+			@PathVariable final String toCurrency) throws CurrencyExchangeNotFoundException {
+		CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(fromCurrency.toUpperCase(), toCurrency.toUpperCase());
+		if (currencyExchange == null) {
+			throw new CurrencyExchangeNotFoundException("Invalid currency pairs");
+		}
 		String port = environment.getProperty("local.server.port");
 		currencyExchange.setEnvironment(port);
 		return currencyExchange;
