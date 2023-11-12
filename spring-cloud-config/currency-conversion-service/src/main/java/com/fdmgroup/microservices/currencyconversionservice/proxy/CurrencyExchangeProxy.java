caddc4f382079bcf677fb3c3e512bd76ecbc6b8f
Pek, Jonetta <jpek009@e.ntu.edu.sg>
2023-11-12 17:43:25 +0800

    create a client for currency exchange service

---
+++ spring-cloud-config/currency-conversion-service/src/main/java/com/fdmgroup/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -0,0 +1,15 @@
+package com.fdmgroup.microservices.currencyconversionservice.proxy;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.fdmgroup.microservices.currencyconversionservice.dao.CurrencyConversion;
+
+@FeignClient(name = "currency-exchange", url = "localhost:8000")
+public interface CurrencyExchangeProxy {
+
+	@GetMapping(path = "/currency-exchange/from/{fromCurrency}/to/{toCurrency}")
+	public CurrencyConversion getExchangeRate(@PathVariable final String fromCurrency,
+			@PathVariable final String toCurrency);
+}
