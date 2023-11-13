9be6230f70470e67938d564ae4603144251e5423
Pek, Jonetta <jpek009@e.ntu.edu.sg>
2023-11-13 15:23:35 +0800

    rename path using router

--- spring-cloud-config/api-gateway/src/main/java/com/fdmgroup/microservices/apigateway/configuration/ApiGatewayConfiguration.java
+++ spring-cloud-config/api-gateway/src/main/java/com/fdmgroup/microservices/apigateway/configuration/ApiGatewayConfiguration.java
@@ -1,11 +1,6 @@
 package com.fdmgroup.microservices.apigateway.configuration;
 
-import java.util.function.Function;
-
-import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.Buildable;
-import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,7 +24,14 @@ public class ApiGatewayConfiguration {
 										.uri("lb://currency-exchange"))
 				.route(predicate -> predicate
 										.path("/currency-conversion/**")
-										.uri("lb://currency-conversion"))				
+										.uri("lb://currency-conversion"))
+//				.route(predicate -> predicate
+//										.path("/currency-conversion-new")
+//										.filters(
+//												filter -> filter.rewritePath("/currency-conversion-new/(?<segment>.*)",
+//																			"/currency-conversion-feign/${segment}")
+//												)
+//										.uri("lb://currency-conversion"))
 				.build();
 	}
 	
