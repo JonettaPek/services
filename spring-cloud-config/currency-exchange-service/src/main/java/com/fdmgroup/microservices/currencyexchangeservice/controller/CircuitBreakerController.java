099be3f9dd3c7854f347a6cdb56c1dcd47cf66c2
Pek, Jonetta <jpek009@e.ntu.edu.sg>
2023-11-13 17:23:29 +0800

    play with rate limiter and bulkhead

--- spring-cloud-config/currency-exchange-service/src/main/java/com/fdmgroup/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ spring-cloud-config/currency-exchange-service/src/main/java/com/fdmgroup/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -2,12 +2,10 @@ package com.fdmgroup.microservices.currencyexchangeservice.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 
 @RestController
 public class CircuitBreakerController {
@@ -15,14 +13,17 @@ public class CircuitBreakerController {
 	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 	
 	@GetMapping(path = "/sample-api")
-//	@Retry(name = "sample-api", fallbackMethod = "fallbackMethod") // default attempts thrice
-	@CircuitBreaker(name = "sample-api", fallbackMethod = "fallbackMethod")
+//	@Retry(name = "sample-api", fallbackMethod = "fallbackMethod") // name = "default" attempts thrice
+//	@CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+//	@RateLimiter(name = "default") // allows 10_000 calls to sample api every 10 secs
+	@Bulkhead(name = "default")
 	public String sampleApi() {
 		logger.info("called sample api");
-		ResponseEntity<String> entity = 
-				new RestTemplate()
-					.getForEntity("http://localhost:8080/dummy-uri", String.class);
-		return entity.getBody();
+//		ResponseEntity<String> entity = 
+//				new RestTemplate()
+//					.getForEntity("http://localhost:8080/dummy-uri", String.class);
+//		return entity.getBody();
+		return "sample-api";
 	}
 	
 	private String fallbackMethod(Exception exception) {
