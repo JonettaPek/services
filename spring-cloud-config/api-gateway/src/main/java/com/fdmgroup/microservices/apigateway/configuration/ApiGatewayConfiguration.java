e71254e48703338b0c0419cc11dceeaa066c9a7b
Pek, Jonetta <jpek009@e.ntu.edu.sg>
2023-11-13 14:52:35 +0800

    configure custom router to shorten uri and remove service name in uri

---
+++ spring-cloud-config/api-gateway/src/main/java/com/fdmgroup/microservices/apigateway/configuration/ApiGatewayConfiguration.java
@@ -0,0 +1,36 @@
+package com.fdmgroup.microservices.apigateway.configuration;
+
+import java.util.function.Function;
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+	@Bean
+	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+		return builder
+				.routes()
+				.route(predicate -> predicate
+										.path("/get")
+										.filters(
+												filter -> filter.addRequestHeader("My-Header", "value") // for authentication purposes
+																.addRequestParameter("request-parameter", "value")
+												)
+										.uri("http://httpbin.org:80/")) // shows details of your request
+				.route(predicate -> predicate
+										.path("/currency-exchange/**")
+										.uri("lb://currency-exchange"))
+				.route(predicate -> predicate
+										.path("/currency-conversion/**")
+										.uri("lb://currency-conversion"))				
+				.build();
+	}
+	
+}
